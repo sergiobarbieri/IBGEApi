18c4f82d2e3a3f7f36bebf8e1f0bd9ed7a6c692d
slbarbieri <sergioluisbarbieri@gmail.com>
2019-11-08 16:34:53 -0200

    Recurso municipio

--- src/main/java/br/com/drummond/resources/MunicipioResources.java
+++ src/main/java/br/com/drummond/resources/MunicipioResources.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,20 @@ public class MunicipioResources {
 	UfRepository ufRepository;
 
 	@RequestMapping(value = "/ufs", method = RequestMethod.GET)
-	public ResponseEntity<List<Uf>> obterCar() {
+	public ResponseEntity<List<Uf>> obterUf() {
+		
+		List<Uf> ufs; 
+		
+		ufs = ufRepository.findAll();
+		if (ufs.isEmpty())
+			return new ResponseEntity<List<Uf>>(ufs, HttpStatus.NO_CONTENT);
+		
+		return new ResponseEntity<List<Uf>>(ufs, HttpStatus.OK);
+	}
+	
+	
+	@RequestMapping(value = "/municipios/{uf}", method = RequestMethod.GET)
+	public ResponseEntity<List<Uf>> obterMunicipio(@PathVariable("uf") String uf ) {
 		
 		List<Uf> ufs; 
 		
